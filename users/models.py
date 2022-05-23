603eb6e92315bd9ec041ffac99409a10b57cb9d9
Mozi <moritz-oettl@web.de>
2022-05-23 22:04:54 +0200

    darkmode backend

--- users/models.py
+++ users/models.py
@@ -5,4 +5,5 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.CharField(max_length=64, null=True, blank=True)
     friends = models.ManyToManyField(User, related_name='friends')
+    white_mode = models.BooleanField(default=False)
 
