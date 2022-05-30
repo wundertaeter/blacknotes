9da5bd48bfbf5b45ea6a03a35473539ef8fa4059
Mozi <moritz-oettl@web.de>
2022-05-30 19:34:11 +0200

    service-worker refactor

--- frontend/src-pwa/custom-service-worker.js
+++ frontend/src-pwa/custom-service-worker.js
@@ -1,57 +1,10 @@
-/*
- * This file (which will be your service worker)
- * is picked up by the build system ONLY if
- * quasar.conf > pwa > workboxPluginMode is set to "InjectManifest"
- */
+import { precacheAndRoute } from "workbox-precaching";
+import { registerRoute } from "workbox-routing";
+import { StaleWhileRevalidate } from "workbox-strategies";
 
-
-// Caching
-// https://medium.com/js-dojo/vuejs-pwa-cache-busting-8d09edd22a31
-
-import { precacheAndRoute, getCacheKeyForURL } from "workbox-precaching";
-import { registerRoute, NavigationRoute } from "workbox-routing";
-import { cacheNames } from "workbox-core";
-import {
-    StaleWhileRevalidate,
-    NetworkFirst,
-    NetworkOnly,
-} from "workbox-strategies";
-
-console.log('ServiceWorker v1');
+console.log('ServiceWorker v2');
 
 // Activate precaching
-precacheAndRoute(self.__WB_MANIFEST, {
-    directoryIndex: null, // Diasable index.html precaching ref: https://developers.google.com/web/tools/workbox/modules/workbox-precaching
-});
-
-// Implements the network-first strategy for navigation routes that falls back to preloaded cache (index.html)
-registerRoute(
-    new NavigationRoute(async ({ url }) => {
-        const cacheName = cacheNames.precache;
-        const cacheKey = getCacheKeyForURL("/index.html");
-        const cache = await caches.open(cacheName);
-
-        let networkResponse;
-        let networkError;
-        try {
-            networkResponse = await fetch(url);
-            if (networkResponse.ok) {
-                await cache.put(cacheKey, networkResponse.clone());
-                return networkResponse;
-            }
-        } catch (error) {
-            networkError = error;
-        }
-
-        const cacheMatch = await cache.match(cacheKey);
-        if (cacheMatch) {
-            return cacheMatch;
-        } else if (networkResponse) {
-            return networkResponse;
-        } else {
-            throw networkError;
-        }
-    })
-);
+precacheAndRoute(self.__WB_MANIFEST);
 
 registerRoute(new RegExp("^http"), new StaleWhileRevalidate());
