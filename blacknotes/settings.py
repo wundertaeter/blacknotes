334269015cd01f572ab356416075c314a777bf85
wundertaeter <moritz-oettl@web.de>
2022-07-02 01:18:17 +0200

    webpush script on server

--- blacknotes/settings.py
+++ blacknotes/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
-    'webpush',
 
 
     'notes',
@@ -170,7 +169,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
--- notify.py
+++ notify.py
@@ -7,8 +7,21 @@ django.setup()
 from users.models import PushNotifications
 from notes.models import Note
 from datetime import date
-
-
+#from webpush.utils import send_to_subscription
+from pywebpush import webpush
+from django.conf import settings
+import json
 notes = Note.objects.filter(when=date.today()).order_by('user__id')
-
 print(notes)
+user = None
+
+for note in notes:
+    if note.user != user:
+        user = note.user
+        subscriptions = PushNotifications.objects.filter(user=user)
+    for sub in subscriptions:
+        print('send message')
+        webpush(subscription_info=sub.subscription,
+                data=json.dumps({'msg': 'Hey'}),
+                vapid_private_key=settings.WEBPUSH_SETTINGS['VAPID_PRIVATE_KEY'],
+                vapid_claims={"sub": 'mailto:' + settings.WEBPUSH_SETTINGS['VAPID_ADMIN_EMAIL']})
