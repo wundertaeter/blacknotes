b8147e9e391d3eb0b4025a4c5ef7b21148c0aba2
Mozi <moritz-oettl@web.de>
2022-07-02 02:14:35 +0200

    notify only allowed on localhost

--- notify.sh
+++ notify.sh
@@ -3,5 +3,5 @@ source .env
 curl --header "Content-Type: application/json" \
   --request POST \
   --data '{"secret":"'"$DJANGO_SECRET"'"}' \
-  https://auth.blacknotes.de/notify/
+  http://localhost:7777/notify/
 
